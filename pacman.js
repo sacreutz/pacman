96c33eaf8948a5bfb04674078647d6b85c472980
sacreutz <sacreutz@gmail.com>
2019-04-11 08:22:26 -0500

    next

--- pacman.js
+++ pacman.js
@@ -26,8 +26,7 @@ function translate (x, y, array){
 
   }
 
-
-
+  function pacman(inputFile){
 
   var fs = require('fs');
 
@@ -36,20 +35,9 @@ function translate (x, y, array){
 
 
 
-   var contents = fs.readFileSync('input.txt', 'utf8');
+   var contents = fs.readFileSync(inputFile, 'utf8');
    var lines = contents.split('\n')
 
-//    function wallTranslate(input){
-//        let walls = input.slice(3)
-//        let newWalls = []
-//        walls.forEach(wall => {
-//             let wallX = wall[0]
-//             let wallY = wall[2]
-//            let newC = translate(wallX, wallY, grid)
-//            newWalls.push(newC)
-//     })
-//     return newWalls
-//    }
 
    let height = Number(lines[0][2])
   let width = Number(lines[0][0])
@@ -79,18 +67,6 @@ function translate (x, y, array){
       grid.push(row);
     }
 
-  //place walls
-// function wallCreation (lines, grid){
-//   let walls = lines.slice(3);
-//   console.log(walls, 'walls')
-//   for (let i = 0; i < walls.length; i++) {
-//       let wall = walls[i]
-//       let wallX = Number(wall[0])
-//       let wallY = Number(wall[2])
-//       let wallToAdd = translate(wallX, wallY, grid)
-//       grid[wallToAdd[0]][wallToAdd[1]] = 'W'
-//   }
-// }
   //place starting point indicator
   let startingCoordinates = lines[1].split(' ')
   let startingX = Number(startingCoordinates[0])
@@ -103,20 +79,6 @@ function translate (x, y, array){
   grid[startingPosition[0]][startingPosition[1]] = 'S'
   let walls =  lines.slice(3);
 
-
-//   function wallTranslate(input){
-//     let walls2 = input.slice(3)
-//     let newWalls = []
-//     walls2.forEach(wall => {
-//          let wallX = wall[0]
-//          let wallY = wall[2]
-//         let newC = translate(wallX, wallY, grid)
-//         newWalls.push(newC)
-//  })
-//  return newWalls
-// }
-//   let translatedWalls = wallTranslate(walls)
-  //console.log(translatedWalls, 'twalls')
 
   walls.forEach(wall => {
     let height2 = Number(lines[0][2])
@@ -139,21 +101,6 @@ function translate (x, y, array){
 
   makeGrid(width, height)
 
-
-//   function wallCreation (linesOne, gridTwo){
-//     let walls = linesOne.slice(3);
-//     console.log(walls, 'walls')
-//     for (let i = 0; i < walls.length; i++) {
-//         let wall = walls[i]
-//         let wallX = Number(wall[0])
-//         let wallY = Number(wall[2])
-//         let wallToAdd = translate(wallX, wallY, grid)
-//         console.log(gridTwo[wallToAdd[0][wallToAdd[1]]])
-//         gridTwo[wallToAdd[0]][wallToAdd[1]] = 'W'
-//     }
-//   }
-
-//   wallCreation(lines, grid)
   let directions = lines[2]
   console.log(directions, 'directions')
   let startingCoordinates = lines[1].split(' ')
@@ -173,19 +120,12 @@ function translate (x, y, array){
 
     let startRow = startingRowIndex
     let startColumn = startingColumnIndex
-    //let check = grid[startingRowIndex][startingColumnIndex]
-
-
     let coins = 0;
-    for (let i = 0; i < directions.length; i++){
-      let check = grid[startRow][startColumn]
-
-
+    for (let i = 0; i < directions.length; i++) {
       console.log('coins', coins)
        console.log(startColumn, 'startColumn')
        console.log(startRow, 'startRow')
       let element = directions[i]
-      console.log(element, 'element')
       if (element === 'N' && startRow > 0 && grid[startRow - 1][startColumn] !== 'W'){
         startRow -= 1;
 
@@ -207,8 +147,6 @@ function translate (x, y, array){
        else if (element === 'S' && startRow < (height - 1) && grid[startRow + 1][startColumn] !== 'W'){
           startRow += 1
 
-
-
            if (grid[startRow][startColumn] !== 'W' && grid[startRow][startColumn] !== 'S'){
          coins += Number(grid[startRow][startColumn]);
         grid[startRow][startColumn] = 0;
@@ -216,8 +154,6 @@ function translate (x, y, array){
        }
         else if (element === 'E' && startColumn < (width - 1) && grid[startRow][startColumn + 1] !== 'W') {
           startColumn += 1
-
-
 
              if (grid[startRow][startColumn] !== 'W' && grid[startRow][startColumn] !== 'S'){
            coins += Number(grid[startRow][startColumn]);
@@ -229,10 +165,13 @@ function translate (x, y, array){
 
       let finalCoordinates = translate2(startRow, startColumn, grid)
       console.log(startRow, startColumn, 'after for loop')
-      returnArray.push(finalCoordinates[0], finalCoordinates[1])
-      returnArray.push(coins)
+      returnArray.push(finalCoordinates[0], finalCoordinates[1], coins)
+        console.log(returnArray, 'returnArray')
       return returnArray;
     }
 
 
-   travel(startingPosition[0], startingPosition[1], directions)
+ return travel(startingPosition[0], startingPosition[1], directions)
+}
+
+  pacman('input.txt')
